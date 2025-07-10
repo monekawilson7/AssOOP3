efccba6c265cfaa93f3727307519287569f74f3f
moneka <monekawilson16@gmail.com>
2025-07-11 02:49:11 +0300

    Add project files.

---
+++ AssOOP3/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssOOP3
+{
+    internal class Calculator
+    {
+        public int Add(int a, int b) {
+            return a + b;
+        }
+        public int Add(int a, int b, int c)
+        {
+            return a + b+ c;
+        }
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ AssOOP3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AssOOP3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            Calculator cal = new Calculator();
+            Console.WriteLine(cal.Add(5, 10));
+            Console.WriteLine(cal.Add(1,2,3));
+            Console.WriteLine(cal.Add(2.5,3.8));
+            #endregion
+        }
+    }
+}
